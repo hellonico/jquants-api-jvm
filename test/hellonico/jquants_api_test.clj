ad42b87ddd38e2bcf2bad0a0a77f982630a24ade
Nicolas Modrzyk <hellonico@gmail.com>
2022-09-14 12:11:49 +0900

    adding test and test files

--- test/hellonico/jquants_api_test.clj
+++ test/hellonico/jquants_api_test.clj
@@ -15,5 +15,16 @@
 
 (deftest statements-test
   (testing "daily code:86970 :date 20220727"
-    (is (= (load-expected "statements_expected.json")
-      (statements {:code 86970 :date 20220727})))))
+    (is (= (load-expected "statements_86970_20220118.json")
+      (statements {:code 86970 :date 20220727}))))
+  (testing "code is invalid"
+    (is (= (load-expected "statements_6digitscode.json")
+           (statements {:code 869701 :date 20220727}))))
+  (testing "code is does not exist"
+    (is (= (load-expected "statements_empty.json")
+           (statements {:code 10000 :date 20220727})))))
+
+(deftest listed-info-test
+  (testing "listed info code:86970"
+    (is (= (load-expected "listed_info_86970.json")
+           (listed-info {:code 86970})))))
