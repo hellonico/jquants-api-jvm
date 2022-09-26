de6ef346c39a29996f5f18f7529fb0b9597c82f5
Nicolas Modrzyk <hellonico@gmail.com>
2022-09-26 18:19:43 +0900

    update version [ci skip]

--- build.clj
+++ build.clj
@@ -4,7 +4,7 @@
             [org.corfield.build :as bb]))
 
 (def lib 'net.clojars.hellonico/jquants-api)
-(def version "0.1.0")
+(def version "0.2.0")
 #_ ; alternatively, use MAJOR.MINOR.COMMITS:
 (def version (format "1.0.%s" (b/git-count-revs nil)))
 
